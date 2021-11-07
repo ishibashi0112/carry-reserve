79ef58e4f29a30d70c53c404f3bc431e65f1a509
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-11-07 23:40:50 +0900

    ✨ firebaseno導入・設定

--- next.config.js
+++ next.config.js
@@ -11,5 +11,14 @@ const withTM = require("next-transpile-modules")([
 ]);
 
 module.exports = withTM({
-  // any other next.js settings here
+  reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: "AIzaSyA7hHZMiIlN8maOktpbPUkDN9wAguRanyY",
+    FIREBASE_AUTH_DOMAIN: "carry-reserve.firebaseapp.com",
+    FIREBASE_DATABESE_URL: "https://carry-reserve.firebaseio.com",
+    FIREBASE_PROJECT_ID: "carry-reserve",
+    FIREBASE_STOREGE_BUCKET: "carry-reserve.appspot.com",
+    FIREBASE_MESSAGING_SENDER_ID: "590633539456",
+    FIREBASE_APP_ID: "1:590633539456:web:f67230dea960840287d365",
+  },
 });
---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABESE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STOREGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const db = getFirestore(firebaseApp);
+// export const auth = getAuth();
