25c7125aba635552290c9cfdf5fee8b08bfb8114
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-11-16 23:51:48 +0900

    💄

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,5 @@
 import { deleteDoc, doc } from "@firebase/firestore";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import EventForm from "src/components/EventFrom";
 import { db } from "src/firebase/firebase";
 import Map from "src/components/Map";
@@ -28,18 +28,14 @@ const List = (props) => {
     await deleteDoc(doc(db, "events", eventId));
   };
 
-  useEffect(() => {
-    console.log(props);
-  });
-
   return (
     <div className="w-full h-full border-2 border-gray-500  mt-16 ">
-      <div className="w-full h-7 bg-gray-300 flex  justify-between  text-center ">
+      <div className="w-full h-7 bg-gray-200 flex  justify-between  text-center ">
         <button
           className={
             view === "確認"
               ? "w-1/3 bg-white  font-bold underline   "
-              : "w-1/3 border-gray-500 font-bold text-gray-400  hover:bg-gray-50 hover:opacity-80 active:opacity-40 "
+              : "w-1/3 border-gray-500 border-b-[0.5px] border-r-[0.5px]  font-bold text-gray-400  hover:bg-gray-50 hover:opacity-80 active:opacity-40 "
           }
           onClick={handleClickTab}
         >
@@ -49,7 +45,7 @@ const List = (props) => {
           className={
             view === "地図"
               ? "w-1/3 bg-white  font-bold underline "
-              : "w-1/3 border-gray-500 font-bold text-gray-400  hover:bg-gray-50 hover:opacity-80 active:opacity-40"
+              : "w-1/3 border-gray-500 border-b-[0.5px] border-r-[0.5px]  border-l-[0.5px] font-bold text-gray-400  hover:bg-gray-50 hover:opacity-80 active:opacity-40"
           }
           onClick={handleClickTab}
         >
@@ -59,7 +55,7 @@ const List = (props) => {
           className={
             view === "追加"
               ? "w-1/3 bg-white  font-bold underline "
-              : "w-1/3 border-gray-500 font-bold text-gray-400  hover:bg-gray-50 hover:opacity-80 active:opacity-40 "
+              : "w-1/3 border-gray-500 border-b-[0.5px] border-l-[0.5px] font-bold text-gray-400  hover:bg-gray-50 hover:opacity-80 active:opacity-40 "
           }
           onClick={handleClickTab}
         >
@@ -72,35 +68,35 @@ const List = (props) => {
             <div className="w-4/5 mx-auto m-3 flex flex-col gap-2">
               <div>
                 <p className=" block text-xs ">行き先</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.destination}
                 </p>
               </div>
 
               <div>
                 <p className=" block text-xs ">日時</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.date}
                 </p>
               </div>
 
               <div>
                 <p className=" block text-xs ">時間帯</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.time_zone}
                 </p>
               </div>
 
               <div>
                 <p className=" block text-xs ">郵便番号</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.zipcode}
                 </p>
               </div>
 
               <div>
                 <p className=" block text-xs ">住所</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.address1}
                   {props.selectEvent.address2}
                 </p>
@@ -108,28 +104,28 @@ const List = (props) => {
 
               <div>
                 <p className=" block text-xs ">電話番号</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.phone_number}
                 </p>
               </div>
 
               <div>
                 <p className=" block text-xs ">担当者</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.key_person}
                 </p>
               </div>
 
               <div>
                 <p className=" block text-xs ">品目情報</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.items}
                 </p>
               </div>
 
               <div>
                 <p className=" block text-xs ">備考（特記事項等）</p>
-                <p className="w-full block border-b outline-none text-lg">
+                <p className="w-full h-[28px] block border-b outline-none text-lg">
                   {props.selectEvent.description}
                 </p>
               </div>
