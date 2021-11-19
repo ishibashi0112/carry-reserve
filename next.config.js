de3cdf4547053544f4c32948f2b4615a3e642df7
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-11-19 23:40:53 +0900

    🐛 google maps API keyを環境変数化

--- next.config.js
+++ next.config.js
@@ -21,5 +21,6 @@ module.exports = withTM({
     FIREBASE_STOREGE_BUCKET: "carry-reserve.appspot.com",
     FIREBASE_MESSAGING_SENDER_ID: "590633539456",
     FIREBASE_APP_ID: "1:590633539456:web:f67230dea960840287d365",
+    GOOGLE_MAPS_API_KEY: "AIzaSyAwujWmsRf6I3YArAfkIkpVJpD2NFqdGJo",
   },
 });
