6d3e2c3adbe92ecddae33b9953535196b1afd7e1
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-24 04:25:53 +0900

    ✨ 洗濯した絵イベント配列にuser_nameを追加する

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,10 +1,17 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from "@fullcalendar/list";
-import { collection, getDocs, onSnapshot } from "@firebase/firestore";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from "@firebase/firestore";
+
 import { db } from "src/firebase/firebase";
 import { eventsState } from "src/stores/valtioState";
 import { useSnapshot } from "valtio";
@@ -12,12 +19,32 @@ import { useSnapshot } from "valtio";
 const Calendar = () => {
   const eventsSnap = useSnapshot(eventsState);
 
-  const handleClickDate = (data) => {
+  const getEventUserName = async (events) => {
+    const addUserNameArray = events.map(async (event) => {
+      const eventUserId = event.extendedProps.user_id;
+      const q = query(
+        collection(db, "users"),
+        where("user_id", "==", eventUserId)
+      );
+      const eventUserDocs = await getDocs(q);
+      const eventUserName = eventUserDocs.docs[0].data().user_name;
+
+      return { ...event, user_name: eventUserName };
+    });
+
+    const results = await Promise.all(addUserNameArray);
+
+    return results;
+  };
+
+  const handleClickDate = async (data) => {
     const dateStr = data.dateStr;
     const DateEvents = eventsSnap.events.filter(
       (event) => dateStr === event.date
     );
-    eventsState.dateEvents = DateEvents;
+    const addUserNameArray = await getEventUserName(DateEvents);
+
+    eventsState.dateEvents = addUserNameArray;
   };
 
   const handleClickEvent = useCallback(
@@ -36,6 +63,7 @@ const Calendar = () => {
         items: e.event.extendedProps.items,
         description: e.event.extendedProps.description,
         isConfirm: e.event.extendedProps.isConfirm,
+        isDone: e.event.extendedProps.isDone,
         user_id: e.event.extendedProps.user_id,
       };
 
@@ -65,6 +93,7 @@ const Calendar = () => {
           items: doc.data().items,
           description: doc.data().description,
           isConfirm: doc.data().isConfirm,
+          isDone: doc.data().isDone,
           user_id: doc.data().user_id,
         },
       }));
@@ -94,6 +123,7 @@ const Calendar = () => {
           items: doc.data().items,
           description: doc.data().description,
           isConfirm: doc.data().isConfirm,
+          isDone: doc.data().isDone,
           user_id: doc.data().user_id,
         },
       }));
@@ -107,27 +137,38 @@ const Calendar = () => {
   }, []);
 
   return (
-    <div className="flex-1 p-6 mx-4 border-[0.5px] border-gray-500 ">
-      <FullCalendar
-        height={"100%"}
-        plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin, listPlugin]}
-        initialView="dayGridMonth"
-        selectable={true}
-        unselectAuto={false}
-        weekends={false}
-        dayMaxEvents={true}
-        headerToolbar={{
-          left: "dayGridMonth,timeGridDay,listMonth",
-          center: "title",
-          right: "prev,next",
-        }}
-        slotMinTime={"07:00:00"}
-        slotMaxTime={"21:00:00"}
-        locale={"ja"}
-        events={eventsSnap.events}
-        dateClick={handleClickDate}
-        eventClick={handleClickEvent}
-      />
+    <div className="w-[700px] resize-x overflow-scroll  h-[600px] p-6 mx-4 border-[0.5px] border-gray-500 ">
+      <div className="w-[700px] h-[600px] ">
+        <FullCalendar
+          height={"100%"}
+          plugins={[
+            timeGridPlugin,
+            dayGridPlugin,
+            interactionPlugin,
+            listPlugin,
+          ]}
+          initialView="dayGridMonth"
+          selectable={true}
+          unselectAuto={false}
+          weekends={false}
+          dayMaxEvents={true}
+          headerToolbar={{
+            left: "dayGridMonth,timeGridDay,listMonth",
+            center: "title",
+            right: "prev,next",
+          }}
+          slotMinTime={"07:00:00"}
+          slotMaxTime={"21:00:00"}
+          locale={"ja"}
+          events={eventsSnap.events}
+          dateClick={handleClickDate}
+          eventClick={handleClickEvent}
+          // eventDidMount={(el) => {
+
+          // }}
+          // defaultAllDay={false}
+        />
+      </div>
     </div>
   );
 };
