86439477f6aa572d8890e368a12e1c1dec0cc9a6
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-25 00:41:21 +0900

    🚚 コンポーネント名の変更

---
+++ src/components/DndArea.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { useSnapshot } from "valtio";
+import { eventsState, mapState, routeListState } from "src/stores/valtioState";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const DndArea = () => {
+  const eventsSnap = useSnapshot(eventsState);
+  const [eventsArray, setEventsArray] = useState(eventsSnap.dateEvents);
+
+  const routeListSnap = useSnapshot(routeListState);
+
+  const handleOnDragEnd = (results) => {
+    const prevArray = eventsArray;
+    const prevIndex = results.source.index;
+    const dragData = prevArray[prevIndex];
+    const newIndex = results.destination ? results.destination.index : null;
+
+    if (prevIndex !== newIndex && newIndex !== null) {
+      const removeArray = prevArray.filter(
+        (event, index) => index != prevIndex
+      ); // 要素の削除
+
+      let newArray = removeArray.reduce((prev, current, index) => {
+        return index !== newIndex
+          ? [...prev, current]
+          : [...prev, dragData, current];
+      }, []); // 要素の追加
+
+      if (newArray.length !== prevArray.length) {
+        newArray = [...newArray, dragData];
+      }
+
+      setEventsArray(newArray);
+    }
+  };
+
+  const handleClickAddButton = () => {
+    const createId = () => {
+      const CheckedArray = eventsArray.filter(
+        (event) => event.destination === "自社"
+      );
+      const id = CheckedArray.length.toString();
+      return id;
+    }; //自社の数に応じてIDを作成
+
+    const createDate = () => {
+      const check = eventsSnap.dateEvents.filter(
+        (event) => event.destination !== "自社"
+      );
+      const date = check[0] ? check[0].date : null;
+      return date;
+    }; //選択中の他イベントと同じdateを作成
+
+    const homeCompanyData = {
+      id: createId(),
+      date: createDate(),
+      zipcode: "2700213",
+      address1: "千葉県野田市桐ヶ作210",
+      address2: "",
+      destination: "自社",
+      phone_number: "0471030606",
+      isConfirm: false,
+      isDone: false,
+      items: "",
+      key_person: "",
+      time_zone: "",
+      user_id: "",
+      description: "",
+    };
+    console.log(homeCompanyData);
+    const addHomeToArray = [...eventsArray, homeCompanyData];
+
+    setEventsArray(addHomeToArray);
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="dropEvents">
+        {(provided, snapshot) => (
+          <ul
+            className={
+              snapshot.isDraggingOver ? "dropEvents opacity-70" : "dropEvents"
+            }
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            <li className="flex justify-around border-b text-gray-400 text-sm font-bold">
+              <p>行き先</p>
+              <p>時間帯</p>
+              <p>品目情報</p>
+              <p>備考</p>
+              <p>予約者</p>
+            </li>
+            {eventsArray?.map((event, index) => (
+              <Draggable key={event.id} draggableId={event.id} index={index}>
+                {(provided, snapshot) => (
+                  <li
+                    className={
+                      snapshot.isDragging
+                        ? "flex justify-around border-b bg-blue-200 shadow-md"
+                        : "flex justify-around border-b "
+                    }
+                    key={event.id}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <p> {event.destination}</p>
+                    <p>{event.time_zone}</p>
+                    <p>{event.items}</p>
+                    <p>{event.description}</p>
+                    <p>{event.user_name}</p>
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+            <button onClick={handleClickAddButton}>自社追加</button>
+            {routeListSnap.switching === "保存" ? (
+              <button
+                onClick={() => routeListState.handleClickButton(eventsArray)}
+              >
+                保存する
+              </button>
+            ) : null}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default DndArea;
