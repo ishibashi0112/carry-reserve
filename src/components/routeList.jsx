7966f958db852938d5aea2135fac9d4038605824
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-26 04:21:37 +0900

    🐛

--- src/components/routeList.jsx
+++ src/components/routeList.jsx
@@ -6,37 +6,24 @@ import DndArea from "src/components/DndArea";
 const RouteList = () => {
   const eventsSnap = useSnapshot(eventsState);
   const routeListSnap = useSnapshot(routeListState);
-  const isConfirm = eventsSnap.dateEvents[0]
     ? eventsSnap.dateEvents[0].isConfirm
-    : null;
+    ? eventsSnap.dateEvents[0].isConfirm
 
   return (
-    <div>
-      <p>ルート情報</p>
-      {/* <button onClick={mapState.clickEventOptimize}>Optimizeボタン</button> */}
-
-      {isConfirm ? (
-        "確定済み"
-      ) : routeListSnap.switching === "編集" ? (
-        <button className="" onClick={routeListState.handleClickButton}>
-          {routeListSnap.switching}
-        </button>
-      ) : null}
-
-      {routeListSnap.switching === "編集" ? (
-        <ul className="dropEvents">
-          <li className="flex justify-around border-b text-gray-400 text-sm font-bold">
-            <p>行き先</p>
-            <p>時間帯</p>
             <p>内容</p>
             <p>予約者</p>
           </li>
           {eventsSnap.dateEvents?.map((event) => (
             <li className="flex justify-around border-b" key={event.id}>
-              <p> {event.destination}</p>
-              <p>{event.time_zone}</p>
+      {isConfirm ? (
+        "確定済み"
+      ) : routeListSnap.switching === "編集" ? (
+        <button className="" onClick={routeListState.handleClickButton}>
+          {routeListSnap.switching}
               <p>{event.items}</p>
-              <p>{event.description}</p>
+      ) : null}
+
+      {routeListSnap.switching === "編集" ? (
             </li>
           ))}
         </ul>
@@ -46,5 +33,8 @@ const RouteList = () => {
     </div>
   );
 };
-
-export default RouteList;
+              <p> {event.destination}</p>
+              <p>{event.time_zone}</p>
+              <p>{event.items}</p>
+              <p>{event.description}</p>
+        <DndArea />
