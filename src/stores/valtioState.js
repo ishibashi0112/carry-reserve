a7f8855cb3fa7a4f2e552185c9fd70333ba6af57
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-25 15:44:59 +0900

    ✨ ルートを確定させ、保存する機能の追加

--- src/stores/valtioState.js
+++ src/stores/valtioState.js
@@ -1,3 +1,13 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "src/firebase/firebase";
 import { proxy } from "valtio";
 
 // イベント
@@ -22,9 +32,78 @@ export const mapState = proxy({
 export const sideBarState = proxy({
   sideBar: false,
   clickEvent: () => {
-    console.log("click");
     sideBarState.sideBar
       ? (sideBarState.sideBar = false)
       : (sideBarState.sideBar = true);
+  },
+});
+
+// ルートリスト
+export const routeListState = proxy({
+  switching: "編集",
+  handleClickButton: async (events) => {
+    switch (routeListState.switching) {
+      case "編集":
+        routeListState.switching = "保存";
+        break;
+      case "保存":
+        events.map(async (event, index) => {
+          if (event.destination === "自社") {
+            await addDoc(collection(db, "events"), {
+              destination: event.destination,
+              date: event.date,
+              time_zone: event.time_zone,
+              zipcode: event.zipcode,
+              address1: event.address1,
+              address2: event.address2,
+              phone_number: event.phone_number,
+              key_person: event.key_person,
+              items: event.items,
+              description: event.description,
+              isConfirm: true,
+              isDone: false,
+              route_order: index + 1,
+              user_id: auth.currentUser.uid,
+            });
+          } else {
+            const eventDoc = doc(db, "events", event.id);
+            await updateDoc(eventDoc, {
+              isConfirm: true,
+              route_order: index + 1,
+            });
+          }
+        });
+
+        const date = events[0].date;
+        const eventsRef = collection(db, "events");
+        const q = query(eventsRef, where("date", "==", date));
+        const res = await getDocs(q);
+        const resArray = res.docs;
+        const eventsArray = resArray.map((doc) => ({
+          id: doc.id,
+          title: doc.data().destination,
+          date: doc.data().date,
+          destination: doc.data().destination,
+          time_zone: doc.data().time_zone,
+          zipcode: doc.data().zipcode,
+          address1: doc.data().address1,
+          address2: doc.data().address2,
+          phone_number: doc.data().phone_number,
+          key_person: doc.data().key_person,
+          phone_number: doc.data().phone_number,
+          items: doc.data().items,
+          description: doc.data().description,
+          isConfirm: doc.data().isConfirm,
+          isDone: doc.data().isDone,
+          route_order: doc.data().route_order,
+          user_id: doc.data().user_id,
+        }));
+
+        // await Promise.all(a);
+        eventsState.dateEvents = eventsArray;
+
+        routeListState.switching = "編集";
+        break;
+    }
   },
 });
