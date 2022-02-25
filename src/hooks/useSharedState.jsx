94734fcb6ef5134dadbc58150f60d082a6710cb1
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-26 04:11:09 +0900

    🔥 ファイル削除

--- src/hooks/useSharedState.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import useSWR from "swr";
-
-export const useSharedState = (key, fallbackData) => {
-  const { data, mutate } = useSWR(key, { fallbackData });
-  return [data, mutate];
-};
