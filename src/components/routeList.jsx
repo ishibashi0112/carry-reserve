bac8fb09836c1ae2ed9e88824a59342b4834dbdb
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-26 04:41:21 +0900

    🐛 再プッシュ

--- src/components/routeList.jsx
+++ src/components/routeList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSnapshot } from "valtio";
 import { eventsState, mapState, routeListState } from "src/stores/valtioState";
 import DndArea from "src/components/DndArea";
@@ -6,24 +6,37 @@ import DndArea from "src/components/DndArea";
 const RouteList = () => {
   const eventsSnap = useSnapshot(eventsState);
   const routeListSnap = useSnapshot(routeListState);
+  const isConfirm = eventsSnap.dateEvents[0]
     ? eventsSnap.dateEvents[0].isConfirm
-    ? eventsSnap.dateEvents[0].isConfirm
+    : null;
 
   return (
-            <p>内容</p>
-            <p>予約者</p>
-          </li>
-          {eventsSnap.dateEvents?.map((event) => (
-            <li className="flex justify-around border-b" key={event.id}>
+    <div>
+      <p>ルート情報</p>
+      {/* <button onClick={mapState.clickEventOptimize}>Optimizeボタン</button> */}
+
       {isConfirm ? (
         "確定済み"
       ) : routeListSnap.switching === "編集" ? (
         <button className="" onClick={routeListState.handleClickButton}>
           {routeListSnap.switching}
-              <p>{event.items}</p>
+        </button>
       ) : null}
 
       {routeListSnap.switching === "編集" ? (
+        <ul className="dropEvents">
+          <li className="flex justify-around border-b text-gray-400 text-sm font-bold">
+            <p>行き先</p>
+            <p>時間帯</p>
+            <p>内容</p>
+            <p>予約者</p>
+          </li>
+          {eventsSnap.dateEvents?.map((event) => (
+            <li className="flex justify-around border-b" key={event.id}>
+              <p> {event.destination}</p>
+              <p>{event.time_zone}</p>
+              <p>{event.items}</p>
+              <p>{event.description}</p>
             </li>
           ))}
         </ul>
@@ -33,8 +46,5 @@ const RouteList = () => {
     </div>
   );
 };
-              <p> {event.destination}</p>
-              <p>{event.time_zone}</p>
-              <p>{event.items}</p>
-              <p>{event.description}</p>
-        <DndArea />
+
+export default RouteList;
