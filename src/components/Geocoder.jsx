28b1ac653771c93657f35a5b5ebc011dfb12a6de
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-26 17:54:19 +0900

    ⚡️ geocoderの処理結果が順不同となっていたため、順番通りの結果を返すよう修正

--- src/components/Geocoder.jsx
+++ src/components/Geocoder.jsx
@@ -4,41 +4,50 @@ import { eventsState, mapState } from "src/stores/valtioState";
 
 import { useSnapshot } from "valtio";
 
+const geocodePromiseResult = (address) => {
+  return new Promise((resolve, reject) => {
+    const geocoder = new window.google.maps.Geocoder();
+    geocoder.geocode(address, (results, status) => {
+      if (status === "OK") {
+        const latLng = {
+          lat: results[0].geometry.location.lat(),
+          lng: results[0].geometry.location.lng(),
+        };
+        resolve(latLng);
+      } else {
+        reject(status);
+      }
+    });
+  });
+};
+
 const Geocoder = () => {
   const eventsSnap = useSnapshot(eventsState);
   const mapSnap = useSnapshot(mapState);
 
   const addressArray = eventsSnap.dateEvents?.map((event) => {
     const address1 = event.address1;
-    const address2 = event.address2;
+    const address2 = event.address2 ? event.address2 : "";
     return {
       address: `${address1}${address2}`,
     };
   });
+  console.log(addressArray);
 
-  const geocodeing = () => {
-    const geocoder = new window.google.maps.Geocoder();
-    let latLngData = [];
-    addressArray?.map((address) => {
-      geocoder.geocode(address, (results, status) => {
-        if (status === "OK") {
-          const latLng = {
-            lat: results[0].geometry.location.lat(),
-            lng: results[0].geometry.location.lng(),
-          };
-          const prevLatLngData = [...latLngData];
-          latLngData = [...prevLatLngData, latLng];
-          if (addressArray.length === latLngData.length) {
-            mapState.latLng = latLngData;
-          }
-        }
-      });
+  const geocodeing = async () => {
+    const promiseResultArray = addressArray?.map(async (address) => {
+      const latLng = await geocodePromiseResult(address);
+      return latLng;
     });
+    const latLngData = await Promise.all(promiseResultArray);
+    mapState.latLng = latLngData;
   };
+
+  console.log(mapState.latLng);
 
   useEffect(() => {
     geocodeing();
-  }, [eventsSnap.dateEvents]);
+  }, [addressArray]);
 
   return (
     <>
