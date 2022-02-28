618766ab741a774ba9130ffc6c6ca5a2e0311678
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-02-28 23:19:54 +0900

    ⚡️ 関数をuseCallBackでメモ化

--- src/components/DndArea.jsx
+++ src/components/DndArea.jsx
@@ -1,38 +1,42 @@
 import React, { useCallback, useState } from "react";
 import { useSnapshot } from "valtio";
-import { eventsState, routeListState } from "src/stores/valtioState";
+import { eventsState, mapState, routeListState } from "src/stores/valtioState";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const DndArea = () => {
   const eventsSnap = useSnapshot(eventsState);
+  const mapSnap = useSnapshot(mapState);
   const [eventsArray, setEventsArray] = useState(eventsSnap.dateEvents);
 
-  const handleOnDragEnd = (results) => {
-    const prevArray = eventsArray;
-    const prevIndex = results.source.index;
-    const dragData = prevArray[prevIndex];
-    const newIndex = results.destination ? results.destination.index : null;
+  const handleOnDragEnd = useCallback(
+    (results) => {
+      const prevArray = eventsArray;
+      const prevIndex = results.source.index;
+      const dragData = prevArray[prevIndex];
+      const newIndex = results.destination ? results.destination.index : null;
 
-    if (prevIndex !== newIndex && newIndex !== null) {
-      const removeArray = prevArray.filter(
-        (event, index) => index != prevIndex
-      ); // 要素の削除
+      if (prevIndex !== newIndex && newIndex !== null) {
+        const removeArray = prevArray.filter(
+          (event, index) => index != prevIndex
+        ); // 要素の削除
 
-      let newArray = removeArray.reduce((prev, current, index) => {
-        return index !== newIndex
-          ? [...prev, current]
-          : [...prev, dragData, current];
-      }, []); // 要素の追加
+        let newArray = removeArray.reduce((prev, current, index) => {
+          return index !== newIndex
+            ? [...prev, current]
+            : [...prev, dragData, current];
+        }, []); // 要素の追加
 
-      if (newArray.length !== prevArray.length) {
-        newArray = [...newArray, dragData];
+        if (newArray.length !== prevArray.length) {
+          newArray = [...newArray, dragData];
+        }
+
+        setEventsArray(newArray);
       }
+    },
+    [eventsArray]
+  );
 
-      setEventsArray(newArray);
-    }
-  };
-
-  const handleClickAdd = () => {
+  const handleClickAddHome = useCallback(() => {
     //⬇︎⬇︎自社の数に応じてIDを作成し、付与
     const createId = () => {
       const CheckedArray = eventsArray.filter(
@@ -70,7 +74,7 @@ const DndArea = () => {
     const addHomeToArray = [...eventsArray, homeData];
 
     setEventsArray(addHomeToArray);
-  };
+  }, [eventsArray, eventsSnap.dateEvents]);
 
   const handleClickBack = useCallback(() => {
     routeListState.switching = "編集";
@@ -157,62 +161,62 @@ const DndArea = () => {
           </li>
         ))}
       </ul>
-    <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId="dropEvents">
-        {(provided, snapshot) => (
-          <ul
-            className={
-              snapshot.isDraggingOver ? "dropEvents opacity-70" : "dropEvents"
-            }
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            <li className="flex justify-around border-b text-gray-400 text-sm font-bold">
-              <p>行き先</p>
-              <p>時間帯</p>
-              <p>品目情報</p>
-              <p>備考</p>
-              <p>予約者</p>
-            </li>
-            {eventsArray?.map((event, index) => (
-              <Draggable key={event.id} draggableId={event.id} index={index}>
-                {(provided, snapshot) => (
-                  <li
-                    className={
-                      snapshot.isDragging
-                        ? "flex justify-around border-b bg-blue-200 shadow-md"
-                        : "flex justify-around border-b "
-                    }
-                    key={event.id}
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    <button data-id={event.id} onClick={handleClickDelete}>
-                      ×
-                    </button>
-                    <p> {event.destination}</p>
-                    <p>{event.time_zone}</p>
-                    <p>{event.items}</p>
-                    <p>{event.description}</p>
-                    <p>{event.user_name}</p>
-                  </li>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-              <button onClick={handleClickAddHome}>自社追加</button>
-            <button onClick={handleClickBack}>戻る</button>
-            <button
-              onClick={() => routeListState.handleClickButton(eventsArray)}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="dropEvents">
+          {(provided, snapshot) => (
+            <ul
+              className={
+                snapshot.isDraggingOver ? "dropEvents opacity-70" : "dropEvents"
+              }
+              {...provided.droppableProps}
+              ref={provided.innerRef}
             >
-              保存する
-            </button>
+              <li className="flex justify-around border-b text-gray-400 text-sm font-bold">
+                <p>行き先</p>
+                <p>時間帯</p>
+                <p>品目情報</p>
+                <p>備考</p>
+                <p>予約者</p>
+              </li>
+              {eventsArray?.map((event, index) => (
+                <Draggable key={event.id} draggableId={event.id} index={index}>
+                  {(provided, snapshot) => (
+                    <li
+                      className={
+                        snapshot.isDragging
+                          ? "flex justify-around border-b bg-blue-200 shadow-md"
+                          : "flex justify-around border-b "
+                      }
+                      key={event.id}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <button data-id={event.id} onClick={handleClickDelete}>
+                        ×
+                      </button>
+                      <p> {event.destination}</p>
+                      <p>{event.time_zone}</p>
+                      <p>{event.items}</p>
+                      <p>{event.description}</p>
+                      <p>{event.user_name}</p>
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+              <button onClick={handleClickAddHome}>自社追加</button>
+              <button onClick={handleClickBack}>戻る</button>
+              <button
+                onClick={() => routeListState.handleClickButton(eventsArray)}
+              >
+                保存する
+              </button>
               {/* <button onClick={handleClickMapUpdate}>地図更新</button> */}
-          </ul>
-        )}
-      </Droppable>
-    </DragDropContext>
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
