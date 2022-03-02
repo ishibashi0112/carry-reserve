507917575f0473c38fd81b9a7ef6b718aa99452f
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-02 23:28:22 +0900

    ✨ react-hot-toastのインストール

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,10 +4,16 @@ import "@fullcalendar/common/main.css";
 import "@fullcalendar/timegrid/main.css";
 import "@fullcalendar/daygrid/main.css";
 import { useAuthCurrentCheck } from "src/hooks/useAuthCurrentCheck";
+import { Toaster } from "react-hot-toast";
 
 const MyApp = ({ Component, pageProps }) => {
   useAuthCurrentCheck();
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Toaster position="top-right" />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
