7bda91c56124e387a9e7c0c78307f0969b67d3c7
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-07 08:19:14 +0900

    🔥

--- src/hooks/useCustomToast.jsx
+++ src/hooks/useCustomToast.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import toast from "react-hot-toast";
 import { deleteEvent } from "src/firebase/firestore";
 import EditForm from "src/components/EditForm";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 const handleClickDismiss = (id) => {
   toast.dismiss(id);
@@ -62,36 +63,6 @@ export const deleteSelectToast = (data) => {
     {
       duration: Infinity,
       position: "top-right",
-    }
-  );
-};
-
-export const editSelectToast = (data) => {
-  toast.custom(
-    (t) => (
-      <div>
-        <div
-          className={`${
-            t.visible ? null : "animate-leave"
-          } block w-screen h-screen z-10 `}
-          aria-hidden={true}
-          onClick={() => handleClickDismiss(t.id)}
-        ></div>
-
-        <div
-          className={`${
-            t.visible ? "animate-enter" : "animate-leave"
-          } ml-auto absolute top-0  right-1/3 mt-10 h-[600px] w-96 bg-white border rounded-md shadow-md z-20`}
-        >
-          <div className="w-full h-hull overflow-scroll">
-            <EditForm event={data} toastId={t.id} />
-          </div>
-        </div>
-      </div>
-    ),
-    {
-      duration: Infinity,
-      position: "top-center",
     }
   );
 };
